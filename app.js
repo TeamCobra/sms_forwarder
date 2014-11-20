4af5fa7a7084368b4797983c3f09018602108a53
Jared Smith <jared.rit@gmail.com>
2014-11-19 20:14:57 -0500

    Add packages for parsing the request object and route for twilio to hit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// code for forwarding the sms in the right
+// format for our mongoose server
+forwardMessage = function(req, res) {
+  // code goes here. use req.body to get text info
+}
+
+// this parses the body of the post request
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// this is the endpoint Twilio will hit
+app.post('/message', forwardMessage);
+
+// this allows for local port of 8080 and 
+// for a hosting service to inject their port
+app.listen(process.env.PORT || 8080);
+
